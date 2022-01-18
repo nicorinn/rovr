e5cb46c2820d2f0d95aa787729021e882ad97d6f
nicorinn <16536987+nicorinn@users.noreply.github.com>
2022-01-18 02:53:11 -0600

    Implemented parallax scroll

--- src/components/imageList/ImageList.tsx
+++ src/components/imageList/ImageList.tsx
@@ -1,17 +1,71 @@
 import { VStack } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { getLatestImages } from '../../api/marsRover.api';
 import { RoverImage } from '../../common/types';
+import { motion, useViewportScroll } from 'framer-motion';
 import ImageCard from '../imageCard';
-
-function displayImages(images: RoverImage[]) {
-  return images.map((img) => {
-    return <ImageCard key={img.id} {...img} />;
-  });
-}
 
 const ImageList: React.FC = () => {
   const [images, setImages] = useState<RoverImage[]>([]);
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const [imageSpace, setImageSpace] = useState(0);
+  const [offset, setOffset] = useState(0);
+  const listRef = useRef<HTMLDivElement | null>(null);
+  const { scrollY } = useViewportScroll();
+
+  // The yProgess value ranges from 0 to 1
+  // Each image is approximately 1/nth of the page
+  useEffect(() => {
+    if (listRef.current) {
+      setImageSpace(listRef.current.scrollHeight / images.length);
+    }
+  }, [images.length]);
+
+  useEffect(
+    () =>
+      scrollY.onChange((y) => {
+        if (imageSpace && y !== 0) {
+          const centeredImgIndex = Math.round(y / imageSpace);
+          if (selectedIndex !== centeredImgIndex) {
+            setSelectedIndex(centeredImgIndex);
+          } else {
+            setOffset(y - selectedIndex * imageSpace);
+          }
+        }
+      }),
+    [scrollY, images.length, selectedIndex, imageSpace]
+  );
+
+  useEffect(() => {
+    console.log(selectedIndex);
+    window.scrollTo({
+      top: imageSpace * selectedIndex,
+    });
+  }, [selectedIndex, images.length, imageSpace]);
+
+  const selectedStyle = {
+    scale: 1,
+    opacity: 1,
+    transition: 'all 200ms ease-in-out',
+    y: offset,
+    zIndex: 999,
+  };
+
+  const unselectedStyle = {
+    scale: 0.5,
+    opacity: 0.5,
+    transition: 'all 200ms ease-in-out',
+  };
+
+  const displayImages = images.map((img, index) => (
+    <motion.div
+      key={img.id}
+      style={selectedIndex === index ? selectedStyle : unselectedStyle}
+      transition={{ ease: 'easeOut', duration: 1 }}
+    >
+      <ImageCard {...img} />
+    </motion.div>
+  ));
 
   useEffect(() => {
     (async () => {
@@ -20,7 +74,11 @@ const ImageList: React.FC = () => {
     })();
   }, []);
 
-  return <VStack spacing={5}>{displayImages(images)}</VStack>;
+  return (
+    <VStack spacing={10} mb={10} ref={listRef}>
+      {displayImages}
+    </VStack>
+  );
 };
 
 export default ImageList;
