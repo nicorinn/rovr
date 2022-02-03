1c4e78a37688110c6e186f48d63cb824ef749d9a
nicorinn <16536987+nicorinn@users.noreply.github.com>
2022-02-02 20:56:03 -0600

    Mars tiles are working

--- src/components/marsGlobe/MarsGlobe.tsx
+++ src/components/marsGlobe/MarsGlobe.tsx
@@ -1,7 +1,39 @@
-import { Viewer } from 'resium';
+import * as Cesium from 'cesium';
+import { Entity, Viewer, PointGraphics } from 'resium';
+
+Cesium.Ion.defaultAccessToken = '';
+
+const position = Cesium.Cartesian3.fromDegrees(-137, -4.9, 100);
+
+const mars = new Cesium.Ellipsoid(3390000, 3390000, 3390000);
+
+const globe = new Cesium.Globe(mars);
+globe.baseColor = new Cesium.Color(173, 98, 66);
+globe.enableLighting = true;
+
+const wmtsMars = new Cesium.WebMapTileServiceImageryProvider({
+  url: 'https://trek.nasa.gov/tiles/Mars/EQ/Mars_Viking_MDIM21_ClrMosaic_global_232m/1.0.0//{Style}/{TileMatrixSet}/{TileMatrix}/{TileRow}/{TileCol}.jpg',
+  style: 'default',
+  layer: 'Mars_Viking_MDIM21_ClrMosaic_global_232m',
+  tileMatrixSetID: 'default028mm',
+  tilingScheme: new Cesium.GeographicTilingScheme({ ellipsoid: mars }),
+});
 
 const MarsGlobe = () => {
-  return <Viewer full></Viewer>;
+  return (
+    <Viewer
+      full
+      imageryProvider={wmtsMars}
+      timeline={false}
+      animation={false}
+      fullscreenButton={false}
+      globe={globe}
+    >
+      <Entity position={position}>
+        <PointGraphics pixelSize={10} />
+      </Entity>
+    </Viewer>
+  );
 };
 
 export default MarsGlobe;
